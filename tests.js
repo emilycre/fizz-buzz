5b26079d958640a64ad5615c03c464ea7d647121
Emily Creamer <emily.creamer@icloud.com>
2019-02-01 15:59:14 -0800

    fixed errors

--- tests.js
+++ tests.js
@@ -1,31 +1,32 @@
 const test = QUnit.test;
 
 function fizzBuzz(number) {
-    if(number % 3 === 0) {
+    if(number % 5 === 0 && number % 3 === 0) {
+        return 'fizzbuzz';
+    }
+
+    else if(number % 3 === 0) {
         return 'fizz';
     }
     
     else if(number % 5 === 0) {
         return 'buzz';    
     }
-
-    else if(number % 3 === 0 && number % 5 === 0)
-        return 'fizzbuzz';
 }
  
 
 test('numbers divisible by 3 replaced by fizz', function(assert) {
-    let result = 3;
-    assert.equal(number, 'fizz'); 
+    const result = fizzBuzz(3);
+    assert.equal(result, 'fizz'); 
 });
 
 test('numbers divisible by 5 replaced by buzz', function(assert) {
-    let result = 5;
-    assert.equal(number, 'buzz');
+    const result = fizzBuzz(5);
+    assert.equal(result, 'buzz');
 });
  
 test('numbers divisible by 3 and 5 replaced by fizzbuzz', function(assert) {
-    let result = 15;
-    assert.equal(number, 'fizzbuzz');
+    const result = fizzBuzz(15);
+    assert.equal(result, 'fizzbuzz');
 
 });
